a3a72b521946b604ceb749bb406e4c32a2ccd636
mxkxx <mxkxx@naver.com>
2024-07-31 12:12:52 +0900

    style: 카드 컴포넌트 반응형

--- containers/dashbord/id/card/Card.tsx
+++ containers/dashbord/id/card/Card.tsx
@@ -8,23 +8,26 @@ function Card({ card }: { card: ICard }) {
   const { title, tags, dueDate, imageUrl } = card;
   return (
     <div className={styles['card']}>
-      {/* <img src='/card_image1.png' alt='image' width={274} height={160} /> */}
-      {imageUrl && (
+      <img src='/card_image1.png' alt='image' width={274} height={160} />
+      {/* {imageUrl && (
         <Image src={imageUrl} alt='image' width={274} height={160} />
-      )}
+      )} */}
       <div className={styles['card-content']}>
         <p className={styles['title']}>{title}</p>
-        <div className={styles['tags']}>
-          {tags.map((tag, idx) => (
-            <ChipCard content={tag} color='orange' key={idx} />
-          ))}
-        </div>
-        <div className={styles['card-bottom']}>
-          <div className={styles['date']}>
-            <IconCalender />
-            <p>{getDate(dueDate)}</p>
+        <div className={styles['card-information']}>
+          <div className={styles['tags']}>
+            {tags.map((tag, idx) => (
+              <ChipCard content={tag} color='orange' key={idx} />
+            ))}
           </div>
-          <div className={styles['profile']}>J</div>
+
+          <div className={styles['card-bottom']}>
+            <div className={styles['date']}>
+              <IconCalender width={18} height={18} />
+              <p>{getDate(dueDate)}</p>
+            </div>
+            <div className={styles['profile']}>J</div>
+          </div>
         </div>
       </div>
     </div>
