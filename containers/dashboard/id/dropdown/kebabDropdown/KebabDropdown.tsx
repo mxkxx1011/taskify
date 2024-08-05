72315d8bce97bdff5b1ffd83161a3f5dcc3979a2
mxkxx <mxkxx@naver.com>
2024-08-05 16:53:50 +0900

    style: 공백 및 프래그먼트 제거

--- containers/dashboard/id/dropdown/kebabDropdown/KebabDropdown.tsx
+++ containers/dashboard/id/dropdown/kebabDropdown/KebabDropdown.tsx
@@ -1,13 +1,10 @@
 import styles from './KebabDropdown.module.scss';
 
-
 export default function KebabDropdown() {
   return (
-    <>
-      <ul className={styles['container']}>
-        <li className={styles['list']}>수정하기</li>
-        <li className={styles['list']}>삭제하기</li>
-      </ul>
-    </>
+    <ul className={styles['container']}>
+      <li className={styles['list']}>수정하기</li>
+      <li className={styles['list']}>삭제하기</li>
+    </ul>
   );
 }
